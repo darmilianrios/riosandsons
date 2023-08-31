c5dfb7c488c671b2c492b318cb0e793e04ca996c
Dennis <d.lindo@gmail.com>
2023-08-31 18:00:03 -0400

    working on watch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
 import * as React from "react"
+import WatchGraphic from "../components/WatchGraphic"
 
 const IndexPage = () => {
-  return <main>hello</main>
+  return (
+    <main className=" bg-gray-400 w-full h-full inset-0 flex justify-center items-center top-0 left-0">
+      <div className=" grid grid-cols-[1fr_auto] bg-white">
+        <div className=" w-[400px] aspect-square border-4">
+          <WatchGraphic />
+        </div>
+        <div>
+          <h1 className="text-4xl">Rios and Sons Watchmers</h1>
+          <h2>Coming Soon</h2>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default IndexPage
-
-export const Head = () => <title>Home Page</title>
